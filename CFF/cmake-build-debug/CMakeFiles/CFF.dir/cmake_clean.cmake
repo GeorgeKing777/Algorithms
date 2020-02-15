623c5d1aa3bd93cc7f36153fb4ecd1593bb0843b
1610512020_张启琳 <1058485422@qq.com>
2020-02-15 14:02:39 +0800

    CFF

---
+++ CFF/cmake-build-debug/CMakeFiles/CFF.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CFF.exe"
+  "CFF.exe.manifest"
+  "CFF.pdb"
+  "CMakeFiles/CFF.dir/main.cpp.obj"
+  "libCFF.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CFF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
